53669df7223f5ce08d368e7682de67f1089555b2
Umuras <akucur@jib.gs>
2021-03-27 13:55:49 +0300

    UI
    UI Ekleme

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+   
+    
+    void Start()
+    {
+        
+    }
+
+    public void Resume()
+    {
+        
+    }
+}
