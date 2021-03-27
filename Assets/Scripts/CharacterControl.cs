56711905d46af35e46cb698a7fe10271d8f49b70
CODESOLE <canerselcuk44@hotmail.com>
2021-03-27 19:51:13 +0300

    lowpoly knight added

--- Assets/Scripts/CharacterControl.cs
+++ Assets/Scripts/CharacterControl.cs
@@ -5,21 +5,34 @@ using UnityEngine;
 public class CharacterControl : MonoBehaviour
 {
     private Animator playerAnim;
+    private Transform initialRotation;
     
     private void Start()
     {
+        initialRotation = gameObject.transform;
         playerAnim = gameObject.GetComponent<Animator>();
     }
 
     // Update is called once per frame
     private void Update()
     {
-        if (Input.GetKey(KeyCode.Space))
-            playerAnim.SetBool("isAttack", true);
+        if (Input.GetKeyUp(KeyCode.Space) && !playerAnim.GetBool("isAttack") && !playerAnim.GetBool("isAttack2") && !playerAnim.GetBool("isDead"))
+        {
+            if (Random.Range(0, 2) == 0)
+            {
+                playerAnim.SetBool("isAttack", true);
+            }
+            else
+            {
+                playerAnim.SetBool("isAttack2", true);
+            }
+        }
     }
 
     public void AttackEnded()
     {
+        gameObject.transform.rotation.Set(initialRotation.rotation.x, initialRotation.rotation.y, initialRotation.rotation.z, initialRotation.rotation.w);
         playerAnim.SetBool("isAttack", false);
+        playerAnim.SetBool("isAttack2", false);
     }
 }
