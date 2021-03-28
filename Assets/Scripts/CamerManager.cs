45e65d91c1ee40feb351abd474607a81a9dbafd5
Muhammet Ali ÖZTÜRK <63191676+muhammedali9778@users.noreply.github.com>
2021-03-28 03:24:45 +0300

    some camera changes + tracking cam

--- Assets/Scripts/CamerManager.cs
+++ Assets/Scripts/CamerManager.cs
@@ -4,17 +4,20 @@ public class CamerManager : MonoBehaviour
 {
     public Transform target;
     public float cameraSpeed = 2.0f;
+    private Vector3 offset;
 
-    private void Start()
+  void Start()
     {
-                
+        offset = transform.position - target.position;    
     }
 
     // Update is called once per frame
-    private void Update()
+   void LateUpdate()
     {
-        transform.position = Vector3.Slerp( new Vector3(transform.position.x, transform.position.y, transform.position.z),
-                                            new Vector3(target.position.x, target.position.y, target.position.z),
-                                           Time.deltaTime * cameraSpeed);
+        //transform.position = Vector3.Slerp( new Vector3(transform.position.x, transform.position.y, transform.position.z),
+        //                                    new Vector3(target.position.x, target.position.y, target.position.z),
+        //                                   Time.deltaTime * cameraSpeed);
+        Vector3 newPosition = new Vector3(transform.position.x, transform.position.y, offset.z+target.position.z);
+        transform.position = Vector3.Lerp(transform.position,newPosition,10*Time.deltaTime);
     }
 }
