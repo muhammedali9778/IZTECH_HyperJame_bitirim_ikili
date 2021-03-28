da21c8508e4468a45a4501b66751bba41aeeac69
CODESOLE <canerselcuk44@hotmail.com>
2021-03-28 18:18:40 +0300

    camera tracking fixed

--- Assets/Scripts/CamerManager.cs
+++ Assets/Scripts/CamerManager.cs
@@ -14,10 +14,10 @@ public class CamerManager : MonoBehaviour
     // Update is called once per frame
    void LateUpdate()
     {
-        //transform.position = Vector3.Slerp( new Vector3(transform.position.x, transform.position.y, transform.position.z),
-        //                                    new Vector3(target.position.x, target.position.y, target.position.z),
-        //                                   Time.deltaTime * cameraSpeed);
-        Vector3 newPosition = new Vector3(transform.position.x, transform.position.y, offset.z+target.position.z);
-        transform.position = Vector3.Lerp(transform.position,newPosition,10*Time.deltaTime);
+        transform.position = Vector3.Slerp( new Vector3(transform.position.x, transform.position.y, transform.position.z),
+                                            new Vector3(target.position.x, target.position.y, target.position.z),
+                                           Time.deltaTime * cameraSpeed);
+        // Vector3 newPosition = new Vector3(transform.position.x, transform.position.y, offset.z+target.position.z);
+        // transform.position = Vector3.Lerp(transform.position,newPosition,10*Time.deltaTime);
     }
 }
--- Assets/Scripts/PauseMenu.cs
+++ Assets/Scripts/PauseMenu.cs
@@ -45,7 +45,6 @@ public class PauseMenu : MonoBehaviour
     public void ExitGame()
     {
         Application.Quit();
-        Debug.Log("Oyundan Çıktın");
     }
     
 }
